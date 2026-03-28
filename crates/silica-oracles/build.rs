f73f87c9add914a973c400318f3b403f327afed1
Nathan Luevano <81777066+Nathan-Luevano@users.noreply.github.com>
2026-03-28 01:19:00 -0400

    feat: implement CapstoneOracle in silica-oracles

--- crates/silica-oracles/build.rs
+++ crates/silica-oracles/build.rs
@@ -6,9 +6,11 @@ fn main() {
         .output()
         .expect("llvm-config --libdir failed; is the micromamba env active?");
     let libdir = String::from_utf8(libdir.stdout).unwrap();
-    let libdir = libdir.trim();
+    let libdir = libdir.trim().to_string();
 
     println!("cargo:rustc-link-search=native={libdir}");
     println!("cargo:rustc-link-lib=dylib=LLVM");
+    println!("cargo:rustc-link-lib=dylib=capstone");
+    println!("cargo:rustc-link-lib=dylib=unicorn");
     println!("cargo:rustc-link-arg=-Wl,-rpath,{libdir}");
 }
